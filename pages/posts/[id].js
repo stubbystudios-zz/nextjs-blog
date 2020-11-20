3b62224f6977d4fd3bee1f4daf67d237461b68cf
Carey Best <careybest@Careys-MBP.attlocal.net>
2020-11-20 14:15:48 -0600

    API setup

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,8 +2,14 @@ import { getAllPostIds, getPostData } from "../../lib/posts";
 
 import Layout from "../../components/layout";
 
-export default function Post() {
-  return <Layout></Layout>;
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <p>{postData.title}</p>
+      <p>{postData.id}</p>
+      <p>{postData.date}</p>
+    </Layout>
+  );
 }
 
 export async function getStaticPaths() {
@@ -11,5 +17,14 @@ export async function getStaticPaths() {
   return {
     paths,
     fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
   };
 }
